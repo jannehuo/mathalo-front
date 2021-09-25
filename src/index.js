b3ff8141fe1fb0e6fb5983bc15d2c61c086ba27a
Aki Santaniemi <santana2@aalto.fi>
2021-09-25 19:28:46 +0300

    shittii

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import Fibonacci from './fibonacci/fibonacci';
 import Prime from './prime/prime';
 
-new Fibonacci();
+
+
+const option = document.getElementById('option')
+
+
+function selectOnChangeHandler(event) {
+    const value = event.target.value
+    if (value === 'Fibonacci') {
+        new Fibonacci()
+    }
+    if (value === 'Prime') {
+        new Prime()
+    }
+}
+
+
+option.addEventListener("change", selectOnChangeHandler, false);
+
+
+/* new Fibonacci();
 new Prime();
+ */
