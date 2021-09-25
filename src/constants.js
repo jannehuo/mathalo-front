d2f9065d70202c162942754c36b7d45370c62db3
Janne Huotari <jannehuo@gmail.com>
2021-09-25 15:06:39 +0300

    Add lodash and some random color palettes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,8 @@
 export const CANVAS_ID = 'canvas'
+export const COLORS = {
+  palettes: [
+    ['#ddf8e8', '#cdd5d1', '#b4a6ab', '#946e83'],
+    ['#4f6d7a', '#c0d6df', '#dbe9ee', '#4a6fa5'],
+    ['#050517', '#cf5c36', '#efc88b', '#f4e3b2'],
+  ],
+}
